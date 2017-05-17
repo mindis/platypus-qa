e661c09198072a76d974cd5f666464713fe3df2a
Tpt <thomaspt@hotmail.fr>
2017-05-17 14:46:19 +0200

    Uses HTTPS in default services URLs

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 # coding=utf-8
 HOST = 'localhost:8000'
-CORE_NLP_URL = 'http://corenlp.askplatyp.us/1.7'
-SYNTAXNET_URL = 'http://syntaxnet.askplatyp.us/v1/parsey-universal-full'
-WIKIDATA_KNOWLEDGE_BASE_URL = 'http://kb.askplatyp.us/api/v1'
+CORE_NLP_URL = 'https://corenlp.askplatyp.us/1.7/'
+SYNTAXNET_URL = 'https://syntaxnet.askplatyp.us/v1/parsey-universal-full'
+WIKIDATA_KNOWLEDGE_BASE_URL = 'https://kb.askplatyp.us/api/v1'
