6ffebdfc201ae418fe5f5bd07c5cc084c0a22d16
Tpt <thomaspt@hotmail.fr>
2017-06-15 15:18:47 +0200

    Uses the right TimeoutError

--- platypus_qa/request_handler.py
+++ platypus_qa/request_handler.py
@@ -21,7 +21,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 import logging
 import time
 import typing
-from concurrent.futures import Future
+from concurrent.futures import Future, TimeoutError
 from concurrent.futures import ThreadPoolExecutor
 from typing import Union, Iterable
 
